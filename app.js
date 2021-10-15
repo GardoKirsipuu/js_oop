9da1afc2a3375aa80517bc01357f35300ebabbeb
Gardo Kirsipuu <gardo1999@gmail.com>
2021-10-15 11:12:04 +0300

    OOJS 1 solved

---
+++ app.js
@@ -0,0 +1,17 @@
+function Shape(name, sides, sideLength) {
+	this.name = name;
+	this.sides = sides;
+	this.sideLength = sideLength;
+	this.calcPerimeter = function(){
+		return this.sides * this.sideLength;
+	}
+
+}
+
+let square = new Shape("square", 4, 5);
+
+console.log(square.calcPerimeter());
+
+let triangle = new Shape("triangle", 3, 3);
+
+console.log(triangle.calcPerimeter());
