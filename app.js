242dffd2c8b0e95ce5875275658c53067c58cecc
Gardo Kirsipuu <gardo1999@gmail.com>
2021-10-15 12:04:05 +0300

    OOJS 3 solved

--- app.js
+++ app.js
@@ -10,12 +10,24 @@ class Shape {
 	}
 }
 
-let square = new Shape("square", 4, 5);
+class Square extends Shape {
+	constructor(sideLength) {
+		super("square" ,4 ,sideLength);
+	}
+	calcArea(){
+		return this.sideLength * this.sideLength
+	}
+}
+
+let square = new Square(5);
+
 
 console.log(square);
 console.log(square.calcPerimeter());
-
+console.log(square.calcArea());
+/*
 let triangle = new Shape("triangle", 3, 3);
 
 console.log(triangle);
 console.log(triangle.calcPerimeter());
+*/
